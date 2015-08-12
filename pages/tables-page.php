5fbd3e923441a486079c2f4a90b6bedf45a306d5
webchimp <rodrigo.tejero@thewebchi.mp>
2015-08-12 09:04:57 -0500

    Pages style and improvements

--- pages/tables-page.php
+++ pages/tables-page.php
@@ -3,199 +3,201 @@
 		<section class="site-section">
 			<h1 class="section-title">Tables</h1>
 
-			<h2 class="title">Getting started</h2>
-			<p>In the beginning there were tables, everything was built using tables. And tables ruled the world ...wide web.</p>
-			<p>Today you may still use tables, so we've created a set of helper classes to make 'em pretty and more useful.</p>
-			<p>Just add the <code>table</code> class to your <code>table</code> elements and <em>voila</em>!</p>
+			<div class="the-content">
+				<h2 class="title">Getting started</h2>
+				<p>In the beginning there were tables, everything was built using tables. And tables ruled the world ...wide web.</p>
+				<p>Today you may still use tables, so we've created a set of helper classes to make 'em pretty and more useful.</p>
+				<p>Just add the <code>table</code> class to your <code>table</code> elements and <em>voila</em>!</p>
 
-			<table class="table">
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-			</table>
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+				</table>
 
-			<h2 class="title">Headers and footers</h2>
-			<p>As you may have guessed, we've included some cool styles for your <code>thead</code> and <code>tfoot</code> tags too just for you.</p>
-			<p>Just use the proper <code>thead</code>, <code>tfoot</code> and <code>th</code> elements.</p>
+				<h2 class="title">Headers and footers</h2>
+				<p>As you may have guessed, we've included some cool styles for your <code>thead</code> and <code>tfoot</code> tags too just for you.</p>
+				<p>Just use the proper <code>thead</code>, <code>tfoot</code> and <code>th</code> elements.</p>
 
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Foo</th>
-						<th>Bar</th>
-						<th>Baz</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-				<tfoot>
-					<tr>
-						<th>Foo</th>
-						<th>Bar</th>
-						<th>Baz</th>
-					</tr>
-				</tfoot>
-			</table>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Foo</th>
+							<th>Bar</th>
+							<th>Baz</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr>
+							<th>Foo</th>
+							<th>Bar</th>
+							<th>Baz</th>
+						</tr>
+					</tfoot>
+				</table>
 
-			<h2 class="title">Zebra stripes</h2>
-			<p>Do you know how the zebra got his stripes? Neither we do!</p>
-			<p>But you can have sweet-striped tables just by adding the <code>table-striped</code> class to your table element. Because you're worth it.</p>
+				<h2 class="title">Zebra stripes</h2>
+				<p>Do you know how the zebra got his stripes? Neither we do!</p>
+				<p>But you can have sweet-striped tables just by adding the <code>table-striped</code> class to your table element. Because you're worth it.</p>
 
-			<table class="table table-striped">
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-			</table>
+				<table class="table table-striped">
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+				</table>
 
-			<h2 class="title">Bordered Table</h2>
-			<p>What about giving border to the table?</p>
-			<p>Not a problem at all, just add the <code>table-border</code> class to your table element.</p>
+				<h2 class="title">Bordered Table</h2>
+				<p>What about giving border to the table?</p>
+				<p>Not a problem at all, just add the <code>table-border</code> class to your table element.</p>
 
-			<table class="table table-border">
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-			</table>
+				<table class="table table-border">
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+				</table>
 
-			<h2 class="title">Hover highlight</h2>
-			<p>We're spoiling you, aren't we?</p>
-			<p>You wanted highlight-on-hover rows and you got 'em. <em>&quot;But how?&quot;</em> you may ask; well, just add the <code>table-hover</code> class to your table and enjoy the pure CSS bonanza.</p>
+				<h2 class="title">Hover highlight</h2>
+				<p>We're spoiling you, aren't we?</p>
+				<p>You wanted highlight-on-hover rows and you got 'em. <em>&quot;But how?&quot;</em> you may ask; well, just add the <code>table-hover</code> class to your table and enjoy the pure CSS bonanza.</p>
 
-			<table class="table table-hover">
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-			</table>
+				<table class="table table-hover">
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+				</table>
 
-			<h2 class="title">Mix and match</h2>
-			<p>Oh holy macaroni!! THIS IS HUGE NEWS!</p>
-			<p><em>You may also combine classes!!</em> Let's see some <code>table-striped</code> &plus; <code>table-hover</code> beauty</p>
+				<h2 class="title">Mix and match</h2>
+				<p>Oh holy macaroni!! THIS IS HUGE NEWS!</p>
+				<p><em>You may also combine classes!!</em> Let's see some <code>table-striped</code> &plus; <code>table-hover</code> beauty</p>
 
-			<table class="table table-striped table-hover">
-				<tbody>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-					<tr>
-						<td>Foo</td>
-						<td>Bar</td>
-						<td>Baz</td>
-					</tr>
-				</tbody>
-			</table>
+				<table class="table table-striped table-hover">
+					<tbody>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+						<tr>
+							<td>Foo</td>
+							<td>Bar</td>
+							<td>Baz</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 
 		</section>
 
